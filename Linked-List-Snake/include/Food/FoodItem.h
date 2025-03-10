14140f1dbf0252fe045b1ba661b3e98bd9700719
Imran1720 <skimran1720@gmail.com>
2025-03-10 11:14:23 +0530

    Implemented food items

---
+++ Linked-List-Snake/include/Food/FoodItem.h
@@ -0,0 +1,36 @@
+#pragma once
+#include"Food/FoodType.h"
+#include "SFML/Graphics.hpp"
+#include "../../include/UI/UIElement/ImageView.h"
+
+using namespace sf;
+using namespace UI::UIElement;
+
+namespace Food
+{
+	class FoodItem
+	{
+	private:
+		ImageView* food_image;
+		FoodType food_type;
+
+		float food_width;
+		float food_height;
+		Vector2i grid_position;
+		
+		void createFoodImage();
+		Vector2f getFoodScreenPosition(Vector2i position);
+		String getFoodTexturePath();
+		void destroy();
+
+	public:
+		FoodItem();
+		~FoodItem();
+		static const int number_of_food = 8;
+		void initialize(Vector2i position, float width, float height, FoodType type);
+		void update();
+		void render();
+
+		FoodType getFoodType();
+	};
+}
---
+++ Linked-List-Snake/source/Food/FoodItem.cpp
@@ -0,0 +1,94 @@
+#include "../../include/Food/FoodItem.h"
+#include "../../include/Global/Config.h"
+#include "../../include/Level/LevelView.h"
+
+using namespace Global;
+using namespace Level;
+
+namespace Food
+{
+	
+	
+	FoodItem::FoodItem() = default;
+	
+	FoodItem::~FoodItem() = default;
+
+	void FoodItem::initialize(Vector2i position, float width, float height, FoodType type)
+	{
+		food_image = nullptr;
+		food_type = type;
+		food_width = width;
+		food_height = height;
+		grid_position = position;
+
+		createFoodImage();
+	}
+	
+	void FoodItem::update()
+	{
+		food_image->update();
+	}
+	
+	void FoodItem::render()
+	{
+		food_image->render();
+	}
+
+	FoodType FoodItem::getFoodType()
+	{
+		return food_type;
+	}
+
+	void FoodItem::createFoodImage()
+	{
+		
+
+		
+		food_image->initialize(getFoodTexturePath(), food_width, food_height, getFoodScreenPosition(grid_position));
+		food_image->show();
+	}
+
+	Vector2f FoodItem::getFoodScreenPosition(Vector2i position)
+	{
+		float food_position_x = LevelView::border_offset_top + (position.x * food_height);
+		float food_position_y = LevelView::border_offset_left + (position.y * food_width);
+
+		return Vector2f(food_position_x,food_position_y);
+	}
+
+	String FoodItem::getFoodTexturePath()
+	{
+		switch (food_type)
+		{
+		case Food::FoodType::APPLE:
+			return Config::apple_texture_path;
+
+		case Food::FoodType::MANGO:
+			return Config::mango_texture_path;     
+
+		case Food::FoodType::ORANGE:
+			return Config::mango_texture_path;     
+
+		case Food::FoodType::PIZZA:
+			return Config::pizza_texture_path;     
+
+		case Food::FoodType::BURGER:
+			return Config::burger_texture_path;     
+
+		case Food::FoodType::CHEESE:
+			return Config::cheese_texture_path;     
+
+		case Food::FoodType::POISION:
+			return Config::poision_texture_path;
+
+		case Food::FoodType::ALCOHOL:
+			return Config::alcohol_texture_path;
+
+		}
+	}
+
+	void FoodItem::destroy()
+	{
+		delete(food_image);
+	}
+}
