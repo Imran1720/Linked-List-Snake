69d480cc49ee7811fadbf073de505f51522b03de
Imran1720 <skimran1720@gmail.com>
2025-03-15 12:28:17 +0530

    Created Double Linked List

--- Linked-List-Snake/include/LinkedListLib/DoubleLinked/DoubleLinkedList.h
+++ Linked-List-Snake/include/LinkedListLib/DoubleLinked/DoubleLinkedList.h
@@ -1,1 +1,38 @@
 #pragma once
+#include "../../include/LinkedListLib/LinkedList.h"
+#include "../../include/LinkedListLib/DoubleLinked/DoubleNode.h"
+
+namespace LinkedListLib
+{
+	namespace DoubleLinked
+	{
+		class DoubleLinkedList : public LinkedList
+		{
+		protected:
+			virtual DoubleNode* createNode() override;
+
+		public:
+			DoubleLinkedList();
+			~DoubleLinkedList();
+
+			void insertNodeAtHead() override;
+			void insertNodeAtMiddle() override;
+			void insertNodeAtTail() override;
+			void insertNodeAtIndex(int index) override;
+
+			void shiftNodesAfterInsertion(DoubleNode* new_node, DoubleNode* cur_node, DoubleNode* prev_node);
+
+
+			void removeAllNode() override;
+			void removeNodeAtHead() override;
+			void removeNodeAtMiddle() override;
+			void removeNodeAtTail() override;
+			void removeNodeAt(int index) override;
+			void removeNodeAtIndex(int index);
+			void removeHalfNodes() override;
+
+			void shiftNodesAfterRemoval(DoubleNode* cur_node);
+			Direction reverse() override;
+		};
+	}
+}
---
+++ Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
@@ -0,0 +1,74 @@
+#include "../../include/LinkedListLib/DoubleLinked/DoubleLinkedList.h"
+
+namespace LinkedListLib
+{
+    namespace DoubleLinked
+    {
+        DoubleLinkedList::DoubleLinkedList() = default;
+
+        DoubleLinkedList::~DoubleLinkedList() = default;
+
+        DoubleNode* DoubleLinkedList::createNode()
+        {
+            return nullptr;
+        }
+
+        void DoubleLinkedList::insertNodeAtHead()
+        {
+        }
+
+        void DoubleLinkedList::insertNodeAtMiddle()
+        {
+        }
+
+        void DoubleLinkedList::insertNodeAtTail()
+        {
+        }
+
+        void DoubleLinkedList::insertNodeAtIndex(int index)
+        {
+        }
+
+        void DoubleLinkedList::shiftNodesAfterInsertion(DoubleNode* new_node, DoubleNode* cur_node, DoubleNode* prev_node)
+        {
+        }
+
+        void DoubleLinkedList::removeAllNode()
+        {
+        }
+
+        void DoubleLinkedList::removeNodeAtHead()
+        {
+        }
+
+        void DoubleLinkedList::removeNodeAtMiddle()
+        {
+        }
+
+        void DoubleLinkedList::removeNodeAtTail()
+        {
+        }
+
+        void DoubleLinkedList::removeNodeAt(int index)
+        {
+        }
+
+        void DoubleLinkedList::removeNodeAtIndex(int index)
+        {
+        }
+
+
+        void DoubleLinkedList::removeHalfNodes()
+        {
+        }
+
+        void DoubleLinkedList::shiftNodesAfterRemoval(DoubleNode* cur_node)
+        {
+        }
+
+        Direction DoubleLinkedList::reverse()
+        {
+            return Direction();
+        }
+    }
+}
